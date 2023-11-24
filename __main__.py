64b491d25102123164d2f6d899d0e6fdeaf65bfc
Gabriel <gabriellopes4769@gmail.com>
2023-11-24 16:25:45 -0300

    feat: model for Aoc2023

--- __main__.py
+++ __main__.py
@@ -1,8 +1,11 @@
 """Aoc2023."""
-from solution import *
 import sys
 
-
-
 if __name__ == "__main__":
-    print(sys.argv)
+    day = int(sys.argv[1])
+    
+    with open(f'./data/{sys.argv[2]}/{day}','r') as f:
+        data = f.read()
+
+    eval(f'from ./solution import Solution_day{day}')
+    solution = eval(f'Solution_day{day}(data)')
---
+++ solution/day0.py
@@ -0,0 +1,6 @@
+"""Solution of day1 - AoC2023."""
+
+
+class Solution_day0(object):   
+    def __init__(self, data) -> None:
+        print('day0')
