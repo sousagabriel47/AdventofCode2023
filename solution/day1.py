e5e966ff4e2e4020d25cafc4168909d37458894b
Gabriel da Silva Lopes Sousa <r323709@opmetro.local>
2023-12-01 02:39:24 -0300

    feat: calibration values two star

---
+++ solution/day1.py
@@ -0,0 +1,50 @@
+"""Solution of day1 - AoC2023."""
+import sys
+
+def read_data(data):
+    data = data.splitlines()
+    sum_data = 0
+    digs = {'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
+    for part in [1,2]:
+        sum_data = 0
+        for line in data:
+            num = []
+            int_num = 0
+            for idx, ch in enumerate(line):
+                if part == 1: 
+                    if ord(ch) <= 57:
+                        num.append(ch)
+                else:
+                    if ord(ch) <= 57:
+                        num.append(ch)
+                    else:
+                        for number in digs.keys():
+                            len_num = len(number)
+                            #print(number, line[idx:idx+len_num])
+                            if line[idx:idx+len_num] == number:
+                                #print(line[idx:idx+len_num], digs[number])
+                                num.append(digs[number])
+            
+            
+            #print(line, num)
+            if len(num):
+                if len(num) > 1:
+                    int_num = int(num[0]+num[-1])
+                else:
+                    int_num = int(num[0]+num[0])
+            if int_num == 0:
+                print(line, num)
+            #print(int_num)
+            sum_data += int_num
+        print(f'part{part}: {sum_data}')
+
+
+
+
+if __name__ == "__main__":
+    nday = 1
+    with open(f'./solution/data/{sys.argv[1]}/{nday}','r') as f:
+        data = f.read()
+    read_data(data)
+
+
