56d3c5ee20ef7a6473f570d8a7d8b210504b4043
Gabriel da Silva Lopes Sousa <r323709@opmetro.local>
2023-12-07 12:55:37 -0300

    feat: day5 try2 part2

--- solution/day5.py
+++ solution/day5.py
@@ -19,17 +19,13 @@ def solve(data):
             transform[name][idT]['interval'] = [st_source,st_source + length - 1]
             transform[name][idT]['delta'] = st_dest - st_source
     for part in [1, 2]:
+        location = []
         if part == 2:
             total = 0
-            for idSeed, seed in enumerate(seeds[::2]):
-                step = seeds[idSeed*2+1]
-                new_seeds = []
-                print(seed, step)
-                total += step
-                for i in range(step):
-                    new_seeds.append(seed + i)
+            for stSeed, rangeSeed in zip(seeds[::2], seeds[1::2]):
+                interval = [stSeed, stSeed+rangeSeed-1]
                 
-                print(min(calc_location(transform, new_seeds)))
+                location.extend([min(calc_location(transform, interval))])
         if part == 1:
             location = calc_location(transform, seeds)
         print(f'part{part}: {min(location)}')
@@ -49,10 +45,28 @@ def calc_location(transform, seeds):
                     num <= intervals['interval'][1]):
                     check = True
                     out[idSeed][idTransform+1] = num + intervals['delta']
+                    break
             if not check:
                 out[idSeed][idTransform+1] = num
         idTransform += 1
     return [line[-1] for line in out]
+
+def calc_location_range(transform, seed_range):
+
+    for _, params in transform.items():
+        list_nums = [line[idTransform] for line in out]
+        for idSeed, num in enumerate(list_nums):
+            check = False
+            for intervals in params.values():
+                if (intervals['interval'][0] <= num and
+                    num <= intervals['interval'][1]):
+                    check = True
+                    out[idSeed][idTransform+1] = num + intervals['delta']
+                    break
+            if not check:
+                out[idSeed][idTransform+1] = num
+        idTransform += 1
+    return []
 
 if __name__ == "__main__":
     nday = 5
