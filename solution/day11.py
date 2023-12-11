fb6c5c3b7c1d4ec7347f0b3dba20f9cc3cdb5512
Gabriel <gabriellopes4769@gmail.com>
2023-12-11 17:57:25 -0300

    feat: day11 Expand 1 x

--- solution/day11.py
+++ solution/day11.py
@@ -10,47 +10,52 @@ def solve(data):
     ans = 0
     for part in [1,2]:
         if part == 1:
-            universe_expand = expandir(universe)
+            step = 1
+        else:
+            step = 2
+        dictExpand = expandir(universe, step)
+        print(dictExpand)
+        listDist = calc_dist(dictExpand)
+        ans = int(sum(listDist)/2)
         print(f'part{part}: {ans}')
         
-def expandir(universe):
+def expandir(universe, step=0):
 
     expand = []
 
     lenL = len(universe)
     lenR = len(universe[0])
-    dupL = 0
-    dupR = 0
-    r = []
+    expR = []
+    expL = []
+
+
+
     for iR in range(lenR):
         if '#' not in [line[iR] for line in universe]:
-            r.append(iR)
-            # newline = universe[iR]
-            # newline.insert(iR + dupR,'.')
-            # expand.append(newline)
-            dupR += 1
-    for iR in range(lenR):
-        newline = universe[iR]
-        newline.insert(iR + dupR,'.')
-        expand.append(newline)
-        dupR += 1
+            expR.append(iR)
     
-    
-    print(r)
-    print_map(expand)
-    print('-----------')
     for iL,line in enumerate(universe):
         if '#' not in line:
-            expand.append(line)
-            expand.append(line)
-            dupL += 1
-        else:
-            expand.append(line)
+            expL.append(iL)
 
 
+    dictGalaxies = {}
+    for iL,line in enumerate(universe):
+        for iR, p in enumerate(line):
+            if p == '#':
+                nExpL = len([eL for eL in expL if iL > eL])
+                nExpR = len([eR for eR in expR if iR > eR])
+                
+                dictGalaxies[f'{iL}_{iR}'] = [iL + nExpL*step, iR + nExpR*step]
+    return dictGalaxies
 
-    print(dupL, dupR)
-    print_map(expand)
+def calc_dist(dictUniverse):
+    dist = []
+
+    for p1 in dictUniverse.values():
+        for p2 in dictUniverse.values():
+            dist.append(abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]))
+    return dist
 
 def print_map(mapa):
     for line in mapa:
