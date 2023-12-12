c261cd1a87d5740f33a1cde47437d23870115617
Gabriel da Silva Lopes Sousa <r323709@opmetro.local>
2023-12-12 05:17:09 -0300

    feat: day12 try1

---
+++ solution/day12.py
@@ -0,0 +1,26 @@
+"""Solution of day1 - AoC2023."""
+import sys
+
+def solve(data):
+    data = data.splitlines()
+    record = [line.split() for line in data]
+    reg = [line[0] for line in record]
+    padrao = [list(map(int,line[1].split(','))) for line in record]
+
+    ans = 0
+    for part in [1,2]:
+        for r, p in zip(reg, padrao):
+            nX = r.count('?')
+            l_padroes = ['#'*n for n in p]
+            print(r, nX, l_padroes)
+        print(f'part{part}: {ans}')
+
+
+if __name__ == "__main__":
+    nday = 12
+
+    with open(f'./solution/data/{sys.argv[1]}/{nday}','r') as f:
+        data = f.read()
+    solve(data)
+
+
