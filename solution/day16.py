638397084b66d6e88d5db1f65d0d3d5f90c59944
Gabriel <gabriellopes4769@gmail.com>
2023-12-17 18:25:17 -0300

    feat: day16 teste complete

--- solution/day16.py
+++ solution/day16.py
@@ -1,32 +1,66 @@
 """Solution of day1 - AoC2023."""
 import sys
 import os
+from collections import deque
+import copy
+from time import sleep
 
 def solve(data):
     mapa = [list(line) for line in data.splitlines()]
     ans = [0,0]
     os.system('cls')
+
+    mov = {'>': {'.':[['>',[0,1]]],'\\':[['v',[1,0]]],'/': [['^', [-1,0]]],'|':[['^', [-1,0]],['v', [1,0]]], '-':[['>', [0,1]]]},
+           '<': {'.':[['<',[0,-1]]],'\\':[['^',[-1,0]]],'/': [['v', [1,0]]],'|':[['^', [-1,0]],['v', [1,0]]], '-':[['<', [0,1]]]},
+           '^': {'.':[['^',[-1,0]]],'\\':[['<',[0,-1]]],'/': [['>', [0,1]]],'|':[['^', [-1,0]]], '-':[['<', [0,-1]],['>', [0,1]]]},
+           'v': {'.':[['v',[1,0]]],'\\':[['>',[0,-1]]],'/': [['<', [0,-1]]],'|':[['v', [1,0]]], '-':[['<', [0,-1]],['>', [0,1]]]}}
+    nL = len(mapa)
+    nR = len(mapa[0])
+    color_map = copy.deepcopy(mapa)
     for part in [1]:
-        caminho = []
+        caminho = deque()
+        caminho_total = []
+        visit = set()
         if part==1:
             p_st = [0,0]
-            v_st = '>'
-            caminho.append([p_st,v_st])
-        print_caminho(mapa, caminho)
+            v_st = [0,1]
+            ch = '>'
+            caminho.append([p_st,v_st,ch])
+            caminho_total.append([p_st, ch])
+            visit.add(f'{p_st[0]}_{p_st[1]}')
+            n_visit = -1
+            for _ in range(1000):
+                p, v, ch = caminho.popleft()
+                os.system('cls')
+                p_atual = [p[0] + v[0], p[1] + v[1]]
+                next_p = mapa[p_atual[0]][p_atual[1]]
+                print(f'{p}  {ch} --> {mapa[p_atual[0]][p_atual[1]]}:{p_atual} ')
+                next_list = mov[ch].get(next_p,[[ch, v]])
+                for next in next_list:
                     
+                    next_ch, next_v = next
+                    next_p = [next_v[0] + p_atual[0],next_v[1] + p_atual[1]]
+                    if next_p[0] >= 0 and next_p[0] < nL and next_p[1] >= 0 and next_p[1] < nR:
+                        caminho.append([p_atual,next_v,next_ch])
+                        caminho_total.append([next_p,next_ch])
+                        visit.add(f'{next_p[0]}_{next_p[1]}')
+                print_caminho(color_map, caminho_total)
+
+            ans[part-1] = len(visit)                  
                 
         print(f'part{part}: {ans[part-1]}')
 
 def print_caminho(mapa, caminho):
+    espelhos = '\/|-'
     for p in caminho:
-        print(p)
-        coor, v = p
-        mapa[coor[0]][coor[1]] = v
+        coor, ch = p
+        if mapa[coor[0]][coor[1]] not in espelhos:
+            mapa[coor[0]][coor[1]] = ch
     print_mapa_color(mapa)
 
 def print_mapa_color(mapa):
     espelhos = '\/|-'
-    caminho = '><^V'
+    caminho = '><^v'
     CEND    = '\33[0m'
     CBLACK  = '\33[30m'
     CRED    = '\33[31m'
