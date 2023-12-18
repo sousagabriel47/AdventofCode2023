2d2d45629a82d7e52705f649e696685f181d9b30
Gabriel <gabriellopes4769@gmail.com>
2023-12-18 20:58:03 -0300

    feat: day18 1st Area

--- solution/day18.py
+++ solution/day18.py
@@ -2,7 +2,6 @@
 import sys
 import os
 from time import sleep
-import matplotlib.pyplot as plt
 from matplotlib.path import Path
 
 
@@ -14,55 +13,57 @@ def solve(data):
     
 
     mov = {'R': [0,1], 'L':[0,-1], 'U': [-1,0], 'D':[1,0]}
-
+    mov2 = ['R', 'D', 'L', 'U']
     for part in [1]:
-        if part==1:
-            caminho = [[0,0]]
-            for cmd, t, color in movimentos:
+        caminho = [[0,0]]
+        for cmd, t, color in movimentos:
+            
+            if part==1:
                 vetor = mov[cmd]
-                for _ in range(int(t)):
-                    p = caminho[-1]
-                    caminho.append([p[0] + vetor[0],p[1] + vetor[1]])
-            # limites
-            listL = [p[0] for p in caminho]
-            listR = [p[1] for p in caminho]
-            
-            limL = [min(listL),max(listL)]
-            limR = [min(listR),max(listR)]
-            # # create mapa
-            # mapa  = [list('.' * (limR[1] - limR[0] + 1)) for _ in range(limL[1] - limL[0] + 1)] 
-            # for p in caminho:
-            #     mapa[p[0]-limL[0]][p[1]-limR[0]] = '#'
-            
-            # #lines
-            # p_count = []
-            # inside = 0
-            # for iL, line in enumerate(mapa):
-            #     prev = '.'
-            #     v = 0
-            #     p_count.append([])
-            #     for iR, ch in enumerate(line):
-            #         if prev != ch and ch == '.':
-            #             v += 1
-            #         prev = ch
-            #         p_count[iL].append(v%10)
-                    
-
-            p = Path(caminho)
-            print(len(caminho))
-            inside = 0
-            for iL in range(limL[0],limL[1]):
-                for iR in range(limR[0],limR[1]):
-                    if [iL, iR] in caminho:
-                        continue
-                    if p.contains_point((iL,iR)):
-                        inside += 1
-
-            ans[part-1] = inside
+            else:
+                new_cmd = mov2[int(color[-2])]
+                new_t = int(color[2:-1],16)+1
+                vetor = mov[new_cmd]
+            steps = int(t) if part == 1 else new_t
+            p = caminho[-1]
+            caminho.append([p[0] + vetor[0]*steps,p[1] + vetor[1]*steps])
+        # limites
+        listL = [p[0] for p in caminho]
+        listR = [p[1] for p in caminho]
+        
+        limL = [min(listL),max(listL)]
+        limR = [min(listR),max(listR)]
+        # create mapa
+        mapa  = [list('.' * (limR[1] - limR[0] + 1)) for _ in range(limL[1] - limL[0] + 1)] 
+        for p in caminho:
+            mapa[p[0]-limL[0]][p[1]-limR[0]] = '#'
+                
 
 
+
+        # p = Path(caminho)
+        # inside = 0
+        # for iL in range(limL[0],limL[1]+1):
+        #     for iR in range(limR[0],limR[1]+1):
+        #         if p.contains_point((iL,iR)) or [iL,iR] in caminho:
+        #             inside += 1
+
+        # ans[part-1] = inside
+        print_mapa_color(mapa)
+        # print(len(caminho))
+        ans[part-1] = Area(caminho)
             
         print(f'part{part}: {ans[part-1]}')
+
+def Area(corners):
+    n = len(corners) # of corners
+    area = 0.0
+    for i in range(n):
+        j = (i + 1) % n
+        area += corners[i][0] * corners[j][1]
+        area -= corners[j][0] * corners[i][1]
+    area = abs(area) / 2.0
+    return area
 
 def print_map(mapa):
     for line in mapa:
