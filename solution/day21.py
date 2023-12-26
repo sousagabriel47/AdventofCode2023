8fb5f458bd9a246e5cf69b0e8a71d23f38b24269
Gabriel da Silva Lopes Sousa <r323709@opmetro.local>
2023-12-26 14:25:40 -0300

    feat: day21 dev

--- solution/day21.py
+++ solution/day21.py
@@ -19,38 +19,38 @@ def solve(data, mode):
 
 
 
-
+    base_rocks = copy(rocks)
     ans = [0,0]
-
-    for part in [1, 2]:
+    mapa_len = [[0, nL], [0, nR]]
+    base_len = copy(mapa_len)
+    for part in [2]:
         garden = st_pos
+        last_garden = [[], copy(garden)]
+        garden_din = [0,0,0]
         if part == 1:
-            for _ in range(10):
-                print(garden)
-                new_garden = next_garden(garden, rocks, nL, nR, part)
-                garden = copy(new_garden)
-            
+            for _ in range(64):
+                garden = next_garden(copy(garden), rocks, nL, nR, part)
             if mode == 'teste':
-                print_mapa_color(nL, nR, rocks, garden)
+                print_mapa_color(mapa_len, rocks, garden)
             ans[part-1] = len(garden)
         else:
-            garden_size = []
-            mapa_len = [[0, nL], [0, nR]]
-            for step in range(10):
-                new_garden = next_garden(garden, rocks, nL, nR, part)
-                garden = copy(new_garden)
-
+            
+            for step in range(200):
+                garden = next_garden(copy(garden), rocks, nL, nR, part)
+                garden_din.append(len(garden))
+                print(step, len(garden), garden_din[-2] - garden_din[-3], garden_din[-1] - garden_din[-2], end=' ')
+                print((garden_din[-2] - garden_din[-3]) - (garden_din[-1] - garden_din[-2]))
                 dir = check_expansao(mapa_len,garden)
                 if dir:
-                    rocks, mapa_len = expandir_rocks(mapa_len, rocks)
-
+                    rocks, mapa_len = expandir_rocks(mapa_len, base_len, base_rocks, rocks, dir)
+                
+                #print_mapa_color(mapa_len, rocks, garden)
                 
 
         print(f'part{part}: {ans[part-1]}')
 
 
-def expandir_rocks(mapa_len ,base_rocks, atual_rocks, dir_exp = []):
-    next_rocks = []
+def expandir_rocks(mapa_len, base_len, base_rocks, atual_rocks, dir_exp = []):
     #[minL,maxL]    [minR,maxR]
 
 
@@ -58,6 +58,32 @@ def expandir_rocks(mapa_len ,base_rocks, atual_rocks, dir_exp = []):
     minL, maxL = lenL
     minR, maxR = lenR
 
+
+    baseL, baseR = base_len
+
+    deltaL = baseL[1] - baseL[0]
+    deltaR = baseR[1] - baseR[0]
+
+
+    if 'D' in dir_exp:
+        for i in range(int((maxR - minR)/deltaR)):
+            atual_rocks += [[rL - deltaL + minL, rR + minR + i*deltaR] for rL, rR in base_rocks]
+        minL = minL - deltaL
+
+
+    if 'U' in dir_exp:
+        for i in range(int((maxR - minR)/deltaR)):
+            atual_rocks += [[rL + maxL,  rR + minR + i*deltaR] for rL, rR in base_rocks]
+        maxL = maxL + deltaL
+
+    if 'L' in dir_exp:
+        for i in range(int((maxL - minL)/deltaL)):
+            atual_rocks += [[rL + minL + i*deltaL, rR - deltaR + minR] for rL, rR in base_rocks]
+        minR = minR - deltaR
+    if 'R' in dir_exp:
+        for i in range(int((maxL - minL)/deltaL)):
+            atual_rocks += [[rL + minL + i*deltaL, rR + maxR] for rL, rR in base_rocks]
+        maxR = maxR + deltaR
 
 
 
@@ -68,7 +94,7 @@ def expandir_rocks(mapa_len ,base_rocks, atual_rocks, dir_exp = []):
     lenR = [minR, maxR]
 
     next_len = [lenL, lenR]
-    return next_rocks, next_len
+    return atual_rocks, next_len
 
 def check_expansao(mapa_len, garden):
     dir_exp = []
@@ -77,10 +103,7 @@ def check_expansao(mapa_len, garden):
     minR, maxR = lenR
     gardenL = [p[0] for p in garden]
     gardenR = [p[1] for p in garden]
-    D_LIM = 1
-    #   
-    #  < 
-    #      
+    D_LIM = 0
     if (min(gardenL) - D_LIM) <= minL:
         dir_exp.append('D')
     if (max(gardenL) + D_LIM) >= maxL:
@@ -92,16 +115,23 @@ def check_expansao(mapa_len, garden):
 
     return dir_exp
 
-def print_mapa_color(nL, nR, rocks, garden):
+def print_mapa_color(mapa_len, rocks, garden):
     CEND    = '\33[0m'
     CWHITE  = '\33[37m'
     CRED    = '\33[31m'
     CRED2    = '\33[41m'
     CGREEN  = '\33[32m'
-    for iL in range(nL):
+
+    lenL, lenR = mapa_len
+    minL, maxL = lenL
+    minR, maxR = lenR
+    print()
+    print()
+
+    for iL in range(minL, maxL):
         line = ''
         print(iL, end='\t')
-        for iR in range(nR):
+        for iR in range(minR, maxR):
             if [iL, iR] in rocks:
                 line += CRED + '#' + CEND
             elif [iL, iR] in garden:
