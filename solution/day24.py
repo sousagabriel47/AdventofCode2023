b8e58083f2e27911603f6a6a579ef7bc3e97d67d
Gabriel <gabriellopes4769@gmail.com>
2023-12-31 08:42:05 -0300

    feat: day24 star1 list-comprehension

--- solution/day24.py
+++ solution/day24.py
@@ -13,9 +13,13 @@ def solve(data, mode):
 
     ans = [0,0]
     for part in [1]:
-        inf = 200000000000000
-        sup = 400000000000000
-        x, y = sp.symbols('x,y')
+        if mode == 'teste':
+            inf = 7
+            sup = 27
+        else:
+            inf = 200000000000000
+            sup = 400000000000000
+        #x, y = sp.symbols('x,y')
         eq = []
         total = 0
         for id, hail in enumerate(hailstones):
@@ -23,41 +27,18 @@ def solve(data, mode):
             p0x, p0y, p0z = p0
             v0x, v0y, v0z = v0
             m0 = v0y/v0x
-            y00 = p0y - m0*p0x
-            eq.append((id, p0x, v0x, sp.Eq(m0*x - y, -y00)))
-        
-        # list_solve = [(sp.solve((eq1, eq2),(x, y))) 
-        #               for idEq1, p0, v0, eq1 in iter(eq) 
-        #               for idEq2, p1, v1, eq2 in iter(eq[idEq1+1:])]
-        # print(list_solve)
-        for idEq1, p0, v0, eq1 in eq:
-            for idEq2, p1, v1, eq2 in eq[idEq1+1:]:
-                a = sp.solve((eq1, eq2),(x, y))
-                if a:
-                    t0 = (a[x] - p0)/v0
-                    t1 = (a[x] - p1)/v1
-                    print(idEq1, idEq2, a[x], a[y], t0, t1)
-                    if ((inf<=a[x]<=sup and inf<=a[y]<=sup) and
-                        t0 > 0 and
-                        t1 > 0):
-                        total += 1
-        ans[part-1] = total
-            # print(hail, y00, m0)
-            # eq0 = sp.Eq(m0*x + y, y00)
-            # for hail2 in [h for h in hailstones if h != hail]:
-            #     p1, v1 = hail2
-            #     p1x, p1y, p1z = p1
-            #     v1x, v1y, v1z = v1
-            #     m1 = v1y/v1x
-            #     y01 = p1y - m1*p1x
-            #     print(hail2, y01, m1)
-                
-            #     eq1 = sp.Eq(m1*x + y, y01)
-            #     an = sp.solve((eq0, eq1),(x, y))
-            #     print(an)
-            #     if m0 != m1:
-            #         x = (y01 - y00)/(m0 - m1)
+            y0 = p0y - m0*p0x
+            if part == 1:
+                eq.append((id, p0x, v0x, m0, y0))
+        if part==1:
+            list_solve = [1 for idEq1, p0, v0, m0, y00 in eq[:-1] for idEq2, p1, v1, m1, y01 in eq[idEq1+1:]
+                        if ((m0 != m1) and
+                            (inf<=(y00-y01)/(m1-m0)<=sup and
+                            inf<=y00 + m0*((y00-y01)/(m1-m0))<=sup) and
+                            ((y00-y01)/(m1-m0) - p0)/v0 > 0 and
+                            ((y00-y01)/(m1-m0) - p1)/v1 > 0)]
 
+        ans[part-1] = sum(list_solve)
 
         print(f'part{part}: {ans[part-1]}')
     
