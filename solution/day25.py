7204db12005eff84431c6118decc586acf16bd8a
Gabriel da Silva Lopes Sousa <r323709@opmetro.local>
2024-01-06 08:42:50 -0300

    feat: day25 Snowverload Complete

--- solution/day25.py
+++ solution/day25.py
@@ -29,48 +29,71 @@ def solve(data, mode):
             if node in listconn:
                 G[node].add(conn)
 
-    print(len(all_nodes))
+    total_nodes = len(all_nodes)
     pairs = set()
     for conn, listconn in G.items():
         for conn2 in listconn:
-            if not((conn2,conn) in pairs and (conn, conn2) in pairs): 
+            if ((conn2,conn) not in pairs and (conn, conn2) not in pairs): 
                 pairs.add((conn, conn2))
 
 
     for part in [1]:
         dist = []
         for st, end in pairs:
-            dist.append([bfs(new_graph(G, (st, end)), st, end), (st, end)])
-        for d in sorted(dist, key=lambda x:x[0]):
-            print(d)
-        print(f'part{part}: {ans[part-1]}')
+            d, n_visit = bfs(new_graph(G, [(st, end)]), st, end)
+            dist.append([d, (st, end)])
+        remove_pair = [p[1] for p in sorted(dist, key=lambda x:x[0])[-3:]]
+        dualG = new_graph(G, remove_pair)
+        t1, t2 = remove_pair[0]
+        _, n1 = bfs(dualG, t1, '')
+        _, n2 = bfs(dualG, t2, '')
+        
+        print(f'part{part}: {n1*n2}')
 
-def new_graph(G, pair):
-    newG = {}
-    n, v = pair
-    for no, vert in G.items():
-        if no == n and len(vert) > 1:
-            newG[no] = {vt for vt in vert if vt!=v}
-        elif no == v and len(vert) > 1:
-            newG[no] = {vt for vt in vert if vt!=n}
-        else:
-            newG[no] = vert
+def new_graph(G, pairs):
+    if len(pairs)>1:
+        newG = {}
+        copyG = deepcopy(G)
+        for pair in pairs:
+            n, v = pair
+            for no, vert in copyG.items():
+                if no == n and len(vert) > 1:
+                    newG[no] = {vt for vt in vert if vt!=v}
+                elif no == v and len(vert) > 1:
+                    newG[no] = {vt for vt in vert if vt!=n}
+                else:
+                    newG[no] = vert
+            copyG=deepcopy(newG)
+    else:
+        newG = {}
+        n, v = pairs[0]
+        for no, vert in G.items():
+            if no == n and len(vert) > 1:
+                newG[no] = {vt for vt in vert if vt!=v}
+            elif no == v and len(vert) > 1:
+                newG[no] = {vt for vt in vert if vt!=n}
+            else:
+                newG[no] = vert
     return newG
 
 def bfs(G, start, end):
     fila = deque([[start, 0]])
     visit = set()
+    nodes = set()
     while fila:
         p, d = fila.popleft()
-
         if p == end:
+            break
+        
+        nodes.add(p)
+        if len(visit) > len(G)*10:
             break
 
         if (p, d) in visit:
             continue
         visit.add((p, d))
         fila.extend([[n, d+1] for n in G[p] if (p, d+1) not in visit])
-    return d
+    return d, len(nodes)
 
 if __name__ == "__main__":
     nday = 25
